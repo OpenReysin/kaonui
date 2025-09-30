a3342c24c585557b76250e4bb921e116c3f8b86b
l.guerra-rago <l.guerra-rago@reysin.fr>
2025-09-30 11:52:59 +0200

    wip

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -1,32 +1,43 @@
 // @ts-check
 
 import starlight from "@astrojs/starlight";
-import { defineConfig } from "astro/config";
+import {defineConfig} from "astro/config";
+import lit from '@astrojs/lit';
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: "KaonUI",
-			social: [
-				{
-					icon: "github",
-					label: "GitHub",
-					href: "https://github.com/withastro/starlight",
-				},
-			],
-			sidebar: [
-				{
-					slug: "introduction",
-				},
-				{
-					slug: "installation",
-				},
-				{
-					label: "Components",
-					autogenerate: { directory: "components" },
-				},
-			],
-		}),
-	],
+    integrations: [
+        starlight({
+            title: "KaonUI",
+            social: [
+                {
+                    icon: "github",
+                    label: "GitHub",
+                    href: "https://github.com/withastro/starlight",
+                },
+            ],
+            sidebar: [
+                {
+                    slug: "introduction",
+                },
+                {
+                    slug: "installation",
+                },
+                {
+                    label: "Components",
+                    autogenerate: {directory: "components"},
+                },
+            ],
+            customCss: [
+                './src/styles/global.css',
+            ]
+        }),
+        lit(),
+    ],
+
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
